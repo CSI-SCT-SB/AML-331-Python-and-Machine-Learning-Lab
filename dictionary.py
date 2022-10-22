3e6aa30c7f5029c2a8c68f2c8469563ade74cb55
Akarsh <79101639+AkarshAnilkumar@users.noreply.github.com>
2022-10-22 19:23:23 +0530

    Add files via upload
    A python code for illustrating dictionaries in python

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+thisdict = {
+     "Name" : "Akarsh",
+     "Number" : 309,
+     "College" : True,
+     "Courses" : ["OS","COA","DBMS","DS"],
+     "Cou" : 304
+}
+print(len(thisdict))
+print(thisdict.keys())
+print(thisdict.values())
